2fa470d1cea688c5173ab396629b630749020695
GrandCircusPaul <paul@grandcircus.com>
2015-07-15 10:22:56 -0400

    Demo List<> Dictionary<>
    Class Demo for Collections : List<> and Dictionary<> are used

---
+++ Demo_Collections/Customer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_Collections
+{
+    class Customer
+    {
+        static int numberOfCustomers;
+        string name;
+        int id;
+
+        public int Id 
+        {
+            get { return this.id; }
+        }
+
+        public Customer()
+        {
+            numberOfCustomers++;
+            this.name = "Customer" + numberOfCustomers;
+            this.id = numberOfCustomers;
+        }
+        public Customer(string _name)
+        {
+            numberOfCustomers++;
+            this.name = _name;
+            this.id = numberOfCustomers;
+        }
+        public void Print()
+        {
+            Console.WriteLine(this.id + "\t" + this.name);
+        }
+    }
+}
---
+++ Demo_Collections/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, Customer> customerDictionary = new Dictionary<int, Customer>();
+            Customer c1 = new Customer();
+            Customer c2 = new Customer("Paul");
+            customerDictionary.Add(c1.Id, c1);
+            customerDictionary.Add(c2.Id, c2);
+
+            customerDictionary[1].Print();
+            customerDictionary[2].Print();
+
+            //customerList.Add(c1);
+            //customerList.Add(c2);
+
+            //foreach (Customer element in customerList)
+            //{
+            //    element.Print();
+            //}
+            
+ 
+
+
+
+
+            //keep console open during debugging
+            Console.ReadLine();
+        }
+        static void testCustomer()
+        {
+            List<Customer> customerList = new List<Customer>();
+            Customer c1 = new Customer();
+            Customer c2 = new Customer("Paul");
+            customerList.Add(c1);
+            customerList.Add(c2);
+
+            foreach (Customer element in customerList)
+            {
+                element.Print();
+            }
+
+        }
+        static void testPoints()
+        {
+            List<Point> pointsList = new List<Point>();
+            Point p1 = new Point();
+            Point p2 = new Point();
+            pointsList.Add(p1);
+            pointsList.Add(p2);
+        }
+    }
+}
